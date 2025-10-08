541191d6312ed4b4c86dc6a69074cc95fb7a8c0f
ottotto0 <dayaogongsong@gmail.com>
2025-10-08 23:46:53 +0900

    Update server.js

--- server.js
+++ server.js
@@ -27,9 +27,6 @@ function chooseImage(text) {
 
 app.listen(3000, () => console.log("Server running on port 3000"));
 
-
-const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 // ★ここを追加！
